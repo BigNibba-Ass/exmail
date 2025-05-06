5b0e74ec6d706eacfa298762ef81d7ba0b7e178e
a1 <betelgeuse1984@outlook.com>
2025-05-06 16:05:50 +0300

    fix

--- resources/js/Components/OfferComponent.vue
+++ resources/js/Components/OfferComponent.vue
@@ -22,17 +22,11 @@ const emit = defineEmits(['download'])
                 <offer-item :offer="props.offer" :user="props.user"/>
             </div>
 
-            <div class="sm:col-span-3">
+            <div class="sm:col-span-6">
                 <button type="button"
                         @click.prevent="emit('download')"
                         class="ms-auto inline-flex w-full text-center justify-center items-center p-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
                     Выгрузить
-                </button>
-            </div>
-            <div class="sm:col-span-3">
-                <button type="button"
-                        class="ms-auto inline-flex w-full text-center justify-center items-center p-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
-                    Отправить
                 </button>
             </div>
         </div>
